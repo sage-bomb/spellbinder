3867fa32365f5415a5512ec03677fe2e506fd5b8
sage-bomb <sage-bomb@users.noreply.github.com>
2025-05-12 00:10:43 -0400

    Added intital UI/UX support (Web based)

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
-
+#testing out the comment injector
 @app.get("/", response_class=HTMLResponse)
-def root(request: Request):
+def root(request: Request): #this is also a comment
     return templates.TemplateResponse("index.html", {"request": request})
+
+
